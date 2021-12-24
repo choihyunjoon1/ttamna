1ee4e444b799482a0967d4abca365b9d1b0da073
seona_min <88869283+seonamin@users.noreply.github.com>
2021-12-24 16:39:01 +0900

    비밀번호 입력값 토글 스크립트 모듈

--- ttamna/src/main/webapp/resources/js/togglePw.js
+++ ttamna/src/main/webapp/resources/js/togglePw.js
@@ -1,6 +1,6 @@
 /**비밀번호 토글
  * type이 password면 text로 바꾸고 text면 password로 바꾼다
- * 헤더에 로그인용 form이 있을 수도 있으니 회원가입form에 id=join-form을 부여
+ * 헤더에 로그인용 form이 있을 수도 있으니 회원가입form에 class=form-check을 부여
  * <CLASS>
  * .toggleInputPw : 비밀번호 입력시 보기/숨김 토글 클래스
  * .input-pw : 비밀번호 입력창 지정 클래스
@@ -9,7 +9,7 @@
  */
 
 	window.addEventListener("load", function(){
-		var form = document.querySelector("#join-form");
+		var form = document.querySelector(".form-check");
         form.querySelector(".toggleInputPw").addEventListener("click", function(){
             var inputPw = form.querySelector(".input-pw");
             if (inputPw.type == "password") {
