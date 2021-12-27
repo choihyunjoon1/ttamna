dac3f784633182f2504a2fb285dee984c4a86738
seona_min <88869283+seonamin@users.noreply.github.com>
2021-12-28 02:44:36 +0900

    Email Service

---
+++ ttamna/src/main/java/com/kh/ttamna/service/member/EmailService.java
@@ -0,0 +1,12 @@
+package com.kh.ttamna.service.member;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+public interface EmailService {
+
+	//인증번호 전송 메소트
+	void sendCertification(String to) throws MessagingException, FileNotFoundException, IOException;
+}
