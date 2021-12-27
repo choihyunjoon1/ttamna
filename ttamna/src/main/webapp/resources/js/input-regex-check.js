b61dc39bb60a1153a7c223660e387faac60bbe73
seona_min <88869283+seonamin@users.noreply.github.com>
2021-12-27 15:39:47 +0900

    정규식 검사 스크립트 설명 추가

--- ttamna/src/main/webapp/resources/js/input-regex-check.js
+++ ttamna/src/main/webapp/resources/js/input-regex-check.js
@@ -3,38 +3,39 @@
  * 회원정보 수정 시에도 적용할 수 있음
  * 각 입력값에 대한 메세지를 보여줄 태그에 입력창별로 클래스를 부여
  * .id-message / .pw-message / .nick-message / .name-message / .phone-message / .email-message
- * 아이디 정규표현식 검사
- * 비밀번호 정규표현식 검사
- * 닉네임 정규표현식 검사
- * 이름 정규표현식 검사
- * 이메일 정규표현식 검사
- * 폰번호 정규표현식 검사
+ * 아이디 정규표현식 검사 - 입력창에 클래스 부여 : .input-id
+ * 비밀번호 정규표현식 검사 - 입력창에 클래스 부여 : .input-pw
+ * 닉네임 정규표현식 검사 - 입력창에 클래스 부여 : .input-nick
+ * 이름 정규표현식 검사 - 입력창에 클래스 부여 : .input-name
+ * 이메일 정규표현식 검사 - 입력창에 클래스 부여 : .input-email
+ * 폰번호 정규표현식 검사 - 입력창에 클래스 부여 : .input-phone
  */
 
     window.addEventListener("load", function(){
       var form  = document.querySelector('.form-check');
 		
       //아이디 정규표현식 검사
-		form.querySelector("input[name=memberId]").addEventListener("input", function(e){
+		form.querySelector(".input-id").addEventListener("input", function(e){
 	        var regex = /^(?=[a-z].*)[a-z0-9_]{4,20}$/;
-	        var inputId = form.querySelector("input[name=memberId]").value;	
+	        var inputId = form.querySelector(".input-id").value;	
 	        var message = form.querySelector(".id-message");
 	        if(inputId != ""){
 	           if(regex.test(inputId)){
 	                console.log("아이디 정규표현식 검사 통과");
 	                message.textContent = "";
+	                
 	            }else{
 	                console.log("아이디 정규표현식 검사 실패");
-	                message.textContent = "영문 소문자, 숫자, 특수문자_ 4~20자 이내로 입력해주세요";
+	                message.textContent = "영문 소문자, 숫자, 특수문자_ 4~20자 이내로 입력해주세요. ";
 					$(form).attr('onsubmit', 'e.preventDefault();');
 	            }
 	        }
    	   });
 		
   		//비밀번호 정규표현식 검사
-  		form.querySelector("input[name=memberPw]").addEventListener("input", function(e){
+  		form.querySelector(".input-pw").addEventListener("input", function(e){
   	        var regex = /^(?=[a-z].*)[a-z0-9_?!@#$%]{4,20}$/;
-  	        var inputPw = form.querySelector("input[name=memberPw]").value;	
+  	        var inputPw = form.querySelector(".input-pw").value;	
   	        var message = form.querySelector(".pw-message");
   	        if(inputPw != ""){
   	           if(regex.test(inputPw)){
@@ -42,16 +43,16 @@
   	                message.textContent = "";
   	            }else{
   	                console.log("비밀번호 정규표현식 검사 실패");
-  	                message.textContent = "영문 소문자, 숫자, 특수문자_!?@#$% 4~20자 이내로 입력해주세요";
+  	                message.textContent = "영문 소문자, 숫자, 특수문자_!?@#$% 4~20자 이내로 입력해주세요. ";
   					$(form).attr('onsubmit', 'e.preventDefault();');
   	            }
   	        }
      	 });
   		
   		//닉네임 정규표현식 검사
-  		form.querySelector("input[name=memberNick]").addEventListener("input", function(e){
+  		form.querySelector(".input-nick").addEventListener("input", function(e){
   	        var regex = /^[가-힣]{2,15}$/;
-  	        var inputNick = form.querySelector("input[name=memberNick]").value;	
+  	        var inputNick = form.querySelector(".input-nick").value;	
   	        var message = form.querySelector(".nick-message");
   	        if(inputNick != ""){
   	           if(regex.test(inputNick)){
@@ -59,16 +60,16 @@
   	                message.textContent = "";
   	            }else{
   	                console.log("닉네임 정규표현식 검사 실패");
-  	                message.textContent = "한글 2~15자 이내로 입력해주세요";
+  	                message.textContent = "한글 2~15자 이내로 입력해주세요. ";
   					$(form).attr('onsubmit', 'e.preventDefault();');
   	            }
   	        }
      	 });
   		
   		//이름 정규표현식 검사
-  		form.querySelector("input[name=memberName]").addEventListener("input", function(e){
+  		form.querySelector(".input-name").addEventListener("input", function(e){
   	        var regex = /^[가-힣]{2,7}$/;
-  	        var inputName = form.querySelector("input[name=memberName]").value;	
+  	        var inputName = form.querySelector(".input-name").value;	
   	        var message = form.querySelector(".name-message");
   	        if(inputName != ""){
   	           if(regex.test(inputName)){
@@ -76,33 +77,34 @@
   	                message.textContent = "";
   	            }else{
   	                console.log("이름 정규표현식 검사 실패");
-  	                message.textContent = "한글 2~7자 이내로 입력해주세요";
+  	                message.textContent = "한글 2~7자 이내로 입력해주세요. ";
   					$(form).attr('onsubmit', 'e.preventDefault();');
   	            }
   	        }
      	 });
   		
   		//이메일 정규표현식 검사
-  		form.querySelector("input[name=memberEmail]").addEventListener("blur", function(e){
+  		form.querySelector(".input-email").addEventListener("input", function(e){
   	        var regex = /^[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*@[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*\.([a-zA-Z])+$/;
-  	        var inputEmail = form.querySelector("input[name=memberEmail]").value;	
+  	        var inputEmail = form.querySelector(".input-email").value;	
   	        var message = form.querySelector(".email-message");
   	        if(inputEmail != ""){
   	           if(regex.test(inputEmail)){
   	                console.log("이메일 정규표현식 검사 통과");
   	                message.textContent = "";
   	            }else{
+  	            	$(".input-email").focus();
   	                console.log("이메일 정규표현식 검사 실패");
-  	                message.textContent = "이메일 형식에 맞지 않습니다";
+  	                message.textContent = "이메일 형식에 맞지 않습니다. ";
   					$(form).attr('onsubmit', 'e.preventDefault();');
   	            }
   	        }
      	 });
 
   		//폰번호 정규표현식 검사
-  		form.querySelector("input[name=memberPhone]").addEventListener("input", function(e){
+  		form.querySelector(".input-phone").addEventListener("input", function(e){
   	        var regex = /^010-[0-9]{4}-[0-9]{4}$/;
-  	        var inputPhone = form.querySelector("input[name=memberPhone]").value;	
+  	        var inputPhone = form.querySelector(".input-phone").value;	
   	        var message = form.querySelector(".phone-message");
   	        if(inputPhone != ""){
   	           if(regex.test(inputPhone)){
@@ -110,7 +112,7 @@
   	                message.textContent = "";
   	            }else{
   	                console.log("폰번호 정규표현식 검사 실패");
-  	                message.textContent = "010-0000-0000 형식으로 입력해 주세요";
+  	                message.textContent = "010-0000-0000 형식으로 입력해 주세요. ";
   					$(form).attr('onsubmit', 'e.preventDefault();');
   	            }
   	        }
