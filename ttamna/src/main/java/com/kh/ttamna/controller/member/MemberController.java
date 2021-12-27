ccaf4b5ce61a6a8b02186dd2d1da3692f497d167
seona_min <88869283+seonamin@users.noreply.github.com>
2021-12-28 02:42:31 +0900

    member Controller

--- ttamna/src/main/java/com/kh/ttamna/controller/member/MemberController.java
+++ ttamna/src/main/java/com/kh/ttamna/controller/member/MemberController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.kh.ttamna.entity.member.MemberDto;
 import com.kh.ttamna.repository.member.MemberDao;
 
