653e00c1760de220d7150fde85c7889b138a9157
rlawhdduq <rlawhdduq95@gmail.com>
2021-12-31 16:31:01 +0900

    게시글 수정 시 이미지 수정(추가, 삭제) 완성

--- ttamna/src/main/java/com/kh/ttamna/service/donation/DonationFileService.java
+++ ttamna/src/main/java/com/kh/ttamna/service/donation/DonationFileService.java
@@ -7,7 +7,7 @@ import com.kh.ttamna.vo.donation.DonationUploadVo;
 public interface DonationFileService {
 
 	int insert(DonationUploadVo donationUploadVo) throws IllegalStateException, IOException;//게시판상세로 바로 가기 위해서 int 반환(게시글 번호)
-	
+	void updateAddFile(DonationUploadVo donationUploadVo) throws IllegalStateException, IOException;
 	void delete(int donationNo);//실제경로에 있는 파일 삭제(게시글 삭제 시)
 	void fileOneDelete(int donationImgNo);//실제경로에 있는 파일 한 개 삭제(수정에서 파일 삭제 시)
 }
