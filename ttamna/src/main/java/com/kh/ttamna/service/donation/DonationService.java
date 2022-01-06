4febd9122ff60ea7835d0527abccbf4bf212c0c1
jammin <mtr0119@naver.com>
2022-01-06 11:24:20 +0900

    상품/기부 결제테이블 병합

--- ttamna/src/main/java/com/kh/ttamna/service/donation/DonationService.java
+++ ttamna/src/main/java/com/kh/ttamna/service/donation/DonationService.java
@@ -2,4 +2,5 @@ package com.kh.ttamna.service.donation;
 
 public interface DonationService {
 	void updatePrice(String sid);
+	void updatePrice(int payNo);
 }
--- ttamna/src/main/java/com/kh/ttamna/service/donation/DonationServiceImple.java
+++ ttamna/src/main/java/com/kh/ttamna/service/donation/DonationServiceImple.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.ttamna.entity.donation.AutoPayMentDto;
+import com.kh.ttamna.entity.payment.PaymentDto;
 import com.kh.ttamna.repository.donation.AutoDonationDao;
 import com.kh.ttamna.repository.donation.DonationDao;
+import com.kh.ttamna.repository.payment.PaymentDao;
 
 @Service
 public class DonationServiceImple implements DonationService{
@@ -16,12 +18,23 @@ public class DonationServiceImple implements DonationService{
 	@Autowired
 	private DonationDao donationDao;
 	
+	@Autowired
+	private PaymentDao paymentDao;
+	
 	@Override//정기기부 해지 시 금액 차감
 	public void updatePrice(String sid) {
 		AutoPayMentDto autoPayMentDto = autoDonationDao.get(sid);
 		
 		int donationNo = autoPayMentDto.getDonationNo();
 		long price = autoPayMentDto.getAutoTotalAmount();
+		donationDao.funding(donationNo, -price);
+	}
+
+	@Override
+	public void updatePrice(int payNo) {
+	 PaymentDto payDto = paymentDao.get(payNo);
+	 int donationNo = payDto.getDonationNo();
+	 long price = payDto.getTotalAmount();
 		donationDao.funding(donationNo, -price);
 	}
 }
