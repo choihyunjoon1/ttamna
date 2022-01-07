7ae25a6726cce84884c2254f90571e8e76e58f4f
seona_min <88869283+seonamin@users.noreply.github.com>
2022-01-07 10:07:47 +0900

    공용 File Path

--- ttamna/src/main/java/com/kh/ttamna/repository/adopt/AdoptImgDaoImpl.java
+++ ttamna/src/main/java/com/kh/ttamna/repository/adopt/AdoptImgDaoImpl.java
@@ -2,6 +2,7 @@ package com.kh.ttamna.repository.adopt;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -19,8 +20,6 @@ public class AdoptImgDaoImpl implements AdoptImgDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	//실제 경로 저장 : FilePath에 상수로 파일 저장경로 설정해 둠
-	String path = FilePath.ADOPT_PATH;
 
 	//입양공고 게시물 이미지 파일 DB저장
 	@Override
@@ -32,7 +31,7 @@ public class AdoptImgDaoImpl implements AdoptImgDao {
 	@Override
 	public void save(AdoptImgDto adoptImgDto, MultipartFile attach) throws IllegalStateException, IOException {
 
-		File target = new File(path, String.valueOf(adoptImgDto.getAdoptImgNo()));
+		File target = new File(FilePath.ADOPT_PATH, String.valueOf(adoptImgDto.getAdoptImgNo()));
 		attach.transferTo(target);
 		sqlSession.insert("adoptImg.save", adoptImgDto);
 		
@@ -60,7 +59,7 @@ public class AdoptImgDaoImpl implements AdoptImgDao {
 	@Override
 	public byte[] load(int adoptImgNo) throws IOException {
 		//파일을 실제 경로에서 불어와 바이트 배열로 보낸다
-		File target = new File(path, String.valueOf(adoptImgNo));
+		File target = new File(FilePath.ADOPT_PATH, String.valueOf(adoptImgNo));
 		byte[] file = FileUtils.readFileToByteArray(target); 
 		return file;
 	}
@@ -70,6 +69,7 @@ public class AdoptImgDaoImpl implements AdoptImgDao {
 	public void dropImg(int adoptImgNo) {
 		sqlSession.delete("adoptImg.dropImg", adoptImgNo);
 	}
+
 
 	//게시글 삭제시 이미지 파일 삭제 처리	
 //	@Override
--- ttamna/src/main/java/com/kh/ttamna/util/FilePath.java
+++ ttamna/src/main/java/com/kh/ttamna/util/FilePath.java
@@ -3,5 +3,14 @@ package com.kh.ttamna.util;
 public class FilePath {
 
 	//adopt 이미지 파일 저장 경로
-	public static final String ADOPT_PATH = "D:/upload/adopt";
+	public static final String ADOPT_PATH = "D:/dev/ttamna/adopt";
+	
+	//donation 이미지 파일 저장 경로
+	public static final String DONATION_PATH = "D:/dev/ttamna/donation";
+
+	//mybaby 이미지 파일 저장 경로
+	public static final String MYBABY_PATH = "D:/dev/ttamna/mtbaby";
+	
+	//shop 이미지 파일 저장 경로
+	public static final String SHOP_PATH = "D:/dev/ttamna/shop";
 }
