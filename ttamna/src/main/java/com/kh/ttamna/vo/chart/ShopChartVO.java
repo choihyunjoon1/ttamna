5183b4d5c4d2d0af444a5b7a7ba83317d1b59568
seona_min <88869283+seonamin@users.noreply.github.com>
2022-01-09 16:15:23 +0900

    주석 수정

--- ttamna/src/main/java/com/kh/ttamna/vo/chart/ShopChartVO.java
+++ ttamna/src/main/java/com/kh/ttamna/vo/chart/ShopChartVO.java
@@ -6,17 +6,17 @@ import lombok.Data;
 public class ShopChartVO {
 
 	private String date; //날짜
-	private long dailyAmount; //일별 기부금액
+	private long dailyAmount; //일별 상품판매 금액
 	
 	private String thisMonthDate; //이번달의 모든 날짜
-	private long thisMonthDailyAmount; //이번달 일별 기부금액
+	private long thisMonthDailyAmount; //이번달 일별 상품판매 금액
 	
 	private String thisMonth; //이번달 
-	private long thisMonthAmount; //이번달 누적 기부금액
+	private long thisMonthAmount; //이번달 누적 상품판매 금액
 	
 	private String monthly; //6개월간의 월
-	private long monthlyAmount; //월별 누적 기부금액
+	private long monthlyAmount; //월별 누적 상품판매 금액
 	
 	private String moy; //12개월간의 월
-	private long moyAmount; //월별 누적 기부금액
+	private long moyAmount; //월별 누적 상품판매 금액
 }
