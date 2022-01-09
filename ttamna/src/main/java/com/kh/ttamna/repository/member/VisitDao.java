1efc609fdc098a727349edd60c8978b136f9c0c5
seona_min <88869283+seonamin@users.noreply.github.com>
2022-01-09 16:15:53 +0900

    회원 방문기록 수정

--- ttamna/src/main/java/com/kh/ttamna/repository/member/VisitDao.java
+++ ttamna/src/main/java/com/kh/ttamna/repository/member/VisitDao.java
@@ -13,11 +13,6 @@ public interface VisitDao {
 	//로그인 시 접속 기록을 저장하는 메소드
 	void log(VisitDto visitDto);
 	
-	//저장한 기록에 중복아이디가 있으면 접속시간을 업데이트
-	//단, 접속일자가 (현재날짜 - 기록날짜) < 1 보다 작다면 접속시간을 업데이트
-	//중복아이디가 아니라면 정보를 등록
-	void allInOneLog(VisitDto visitDto);
-	
 	//오늘 하루의 방문자 수를 계산하는 메소드
 	int countByDay();
 	
