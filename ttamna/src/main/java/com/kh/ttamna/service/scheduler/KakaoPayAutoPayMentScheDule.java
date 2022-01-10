054c844008442cf0df5792d1deb40021c6874c69
rlawhdduq <rlawhdduq95@gmail.com>
2022-01-10 20:14:28 +0900

    1. 기부게시판 등록, 수정, 삭제 인터셉터 적용 2. 정기기부결제 시 확인용 이메일 발송 처리

--- ttamna/src/main/java/com/kh/ttamna/service/scheduler/KakaoPayAutoPayMentScheDule.java
+++ ttamna/src/main/java/com/kh/ttamna/service/scheduler/KakaoPayAutoPayMentScheDule.java
@@ -1,8 +1,12 @@
 package com.kh.ttamna.service.scheduler;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URISyntaxException;
+
+import javax.mail.MessagingException;
 
 public interface KakaoPayAutoPayMentScheDule {
 
-	void excute() throws URISyntaxException;
+	void excute() throws URISyntaxException, FileNotFoundException, MessagingException, IOException;
 }
