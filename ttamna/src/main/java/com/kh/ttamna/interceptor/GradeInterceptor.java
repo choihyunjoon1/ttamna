b95502c29d87ecb325b44afed7cd5e019a89a37d
rlawhdduq <rlawhdduq95@gmail.com>
2022-01-14 17:35:21 +0900

    gradeinterceptor로 병합

--- ttamna/src/main/java/com/kh/ttamna/interceptor/GradeInterceptor.java
+++ ttamna/src/main/java/com/kh/ttamna/interceptor/GradeInterceptor.java
@@ -25,7 +25,7 @@ public class GradeInterceptor implements HandlerInterceptor{
 			return true;
 		}else {
 			System.out.println("[Grade Interceptor] " + memberGrade + " 등급. 접속 차단. " );
-			response.sendRedirect(request.getContextPath());
+			response.sendRedirect(request.getContextPath()+"/donation/");
 			return false;
 		}
 	}
