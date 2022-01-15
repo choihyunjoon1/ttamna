b32d24334556da553dbbc47f3e66c9c7bd22bdcb
seona_min <88869283+seonamin@users.noreply.github.com>
2022-01-16 00:57:20 +0900

    주석 수정

--- ttamna/src/main/java/com/kh/ttamna/controller/HomeController.java
+++ ttamna/src/main/java/com/kh/ttamna/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.kh.ttamna.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,19 +12,19 @@ public class HomeController {
 	}
 	
 	//보호소 등급 신청 방법 안내 페이지
-	@RequestMapping("/upgrade")
+	@GetMapping("/upgrade")
 	public String upgrade() {
 		return "upgrade";
 	}
 	
 	//정부지원 유기동물 입양비 지원 안내 페이지
-	@RequestMapping("/support")
+	@GetMapping("/support")
 	public String support() {
 		return "support";
 	}
 
-	//정부지원 유기동물 입양비 지원 안내 페이지
-	@RequestMapping("/about_us")
+	//사이트 소개 페이지
+	@GetMapping("/about_us")
 	public String aboutUs() {
 		return "about_us";
 	}
