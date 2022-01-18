3ae45cab953bc7426ed16473cba8b3a9ef86d065
choihyunjoon1 <992715@naver.com>
2022-01-18 21:47:45 +0900

    휴면계정 약간 수정

---
+++ ttamna/src/main/java/com/kh/ttamna/controller/mybaby/MybabyAjaxController.java
@@ -0,0 +1,13 @@
+package com.kh.ttamna.controller.mybaby;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mybaby/ajax")
+public class MybabyAjaxController {
+	
+	
+	
+	
+}
